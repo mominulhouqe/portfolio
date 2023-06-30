9237f3ce4c12cbae1c78d568a4b71a0d38ba9915
mominul <ulhouqemomin39@gmail.com>
2023-06-30 15:50:35 +0600

    updated blog

--- src/pages/Home/Home/BlogSection.jsx
+++ src/pages/Home/Home/BlogSection.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { FaEye } from 'react-icons/fa';
+import { Fade } from 'react-awesome-reveal';
+
 
 const BlogSection = () => {
     const blogItems = [
@@ -8,88 +11,95 @@ const BlogSection = () => {
             title: 'Education: A Podpourri of Learning Options: Pods',
             image: 'https://i.ibb.co/r72xf0J/my-img7.jpg',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget ligula ac ante scelerisque eleifend.',
-            date: 'April 15, 2023'
+            date: 'April 15, 2023',
+            views: 1000,
         },
         {
             title: 'Exploring the World of Online Education',
             image: 'https://i.ibb.co/7XXJCgb/my-imgf01.webp',
             content: 'Fusce sed neque ac massa luctus elementum eu vel eros. Integer in consectetur mi, vitae consequat ipsum.',
-            date: 'May 5, 2023'
+            date: 'May 5, 2023',
+            views: 500,
         },
         {
             title: 'The Power of Lifelong Learning',
             image: 'https://i.ibb.co/3r1fTmX/my-imgf05.webp',
             content: 'Curabitur blandit, sapien vel convallis pretium, sapien tellus commodo mi, in egestas odio ligula vel ipsum.',
-            date: 'Jun 12, 2023'
+            date: 'Jun 12, 2023',
+            views: 750,
         },
         {
             title: 'Unlocking Your Potential Through Education',
             image: 'https://i.ibb.co/KN6CJ1P/my-imgf06.webp',
             content: 'Pellentesque tincidunt lectus in fringilla consectetur. Etiam tincidunt tellus eget tempor consequat.',
-            date: 'July 1, 2023'
+            date: 'July 1, 2023',
+            views: 1200,
         },
         {
             title: 'The Future of Education: Technology and Innovation',
             image: 'https://i.ibb.co/1n164tQ/my-imgf04.webp',
             content: 'Suspendisse vitae ipsum vitae arcu sodales placerat. Nullam vitae aliquet magna, eu scelerisque turpis.',
-            date: 'Aug 3, 2023'
+            date: 'Aug 3, 2023',
+            views: 900,
         },
         {
             title: 'The Role of Education in Shaping Society',
             image: 'https://i.ibb.co/HTtph5B/my-imgf03.webp',
             content: 'Vestibulum non lacus a urna bibendum volutpat eu sit amet est. Sed at feugiat velit.',
-            date: 'Sept 4, 2023'
+            date: 'Sept 4, 2023',
+            views: 1500,
         },
     ];
 
-
     return (
         <section className="bg-[#FDF8F7] py-10">
-            <div className="container mx-auto">
-                <div className="text-center">
-                    <h3 className="text-4xl font-bold text-gray-800 mb-2">From My Blog</h3>
-                    <p className="text-gray-600 mb-3">Our Recent Updates, Blog, Tips, Tricks & More</p>
-                    <hr />
+        <div className="container mx-auto">
+          <div className="text-center">
+            <h3 className="text-4xl font-bold text-gray-800 mb-2">From My Blog</h3>
+            <p className="text-gray-600 mb-3">Our Recent Updates, Blog, Tips, Tricks & More</p>
+            <hr />
+          </div>
+  
+          <Carousel
+            showArrows={true}
+            infiniteLoop={true}
+            emulateTouch={true}
+            showThumbs={false}
+            showIndicators={false}
+            autoPlay={true}
+            interval={5000}
+            transitionTime={500}
+          >
+            {blogItems.map((item, index) => (
+              <Fade key={index} direction="up" delay={index * 200}>
+                <div className="relative">
+                  <h2 className="text-3xl font-semibold text-center mb-8">{item.date}</h2>
+                  <div className="flex flex-col md:flex-row items-center gap-8">
+                    <div className="w-full md:w-1/2">
+                      <img
+                        className="rounded-lg h-64 w-full object-cover"
+                        src={item.image}
+                        alt="Blog Image"
+                      />
+                    </div>
+                    <div className="w-full md:w-1/2 relative">
+                      <h3 className="text-xl font-semibold text-gray-800 mb-4">{item.title}</h3>
+                      <p className="text-gray-600 mb-4">{item.content}</p>
+                      <div className="flex items-center absolute bottom-4 right-4">
+                        <FaEye className="text-gray-600 mr-2" />
+                        <p className="text-gray-600 mb-4">{item.views}</p>
+                      </div>
+                      <a href="#" className="text-blue-500 hover:text-blue-700 font-semibold">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
                 </div>
-
-                <Carousel
-                    showArrows={true}
-                    infiniteLoop={true}
-                    emulateTouch={true}
-                    showThumbs={false}
-                    showIndicators={false}
-                    autoPlay={true}
-                    interval={5000}
-                    transitionTime={500}
-                >
-                    {blogItems.map((item, index) => (
-                        <div>
-                            <h2 className="text-3xl font-semibold text-center mb-8">{item.date}</h2>
-                            <div key={index} className="flex flex-col md:flex-row items-center gap-8">
-
-                                <div className="w-full md:w-1/2">
-                                    <img
-                                        className="rounded-lg h-64 w-full object-cover"
-                                        src={item.image}
-                                        alt="Blog Image"
-                                    />
-                                </div>
-                                <div className="w-full md:w-1/2">
-                                    <h3 className="text-xl font-semibold text-gray-800 mb-4">{item.title}</h3>
-                                    <p className="text-gray-600 mb-4">{item.content}</p>
-                                    <a
-                                        href="#"
-                                        className="text-blue-500 hover:text-blue-700 font-semibold"
-                                    >
-                                        Read More
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </Carousel>
-            </div>
-        </section>
+              </Fade>
+            ))}
+          </Carousel>
+        </div>
+      </section>
     );
 };
 
--- src/pages/Home/Home/Contact.jsx
+++ src/pages/Home/Home/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineSend, AiOutlineHome, AiOutlinePhone, AiOutlineMail } from 'react-icons/ai';
+import { Fade } from 'react-awesome-reveal';
 
 const Contact = () => {
   const [name, setName] = useState('');
@@ -43,83 +44,87 @@ const Contact = () => {
         <h2 className="text-3xl font-bold text-gray-800 mb-8">Contact Us</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="bg-white rounded-lg shadow-md p-6">
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">Send us a message</h3>
-            <form onSubmit={handleFormSubmit}>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                  Name
-                </label>
-                <input
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Your name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                  <AiOutlineMail className="inline-block mr-2" />
-                  Email
-                </label>
-                <input
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
-                  <AiOutlineSend className="inline-block mr-2" />
-                  Message
-                </label>
-                <textarea
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                  id="message"
-                  name="message"
-                  rows="5"
-                  placeholder="Your message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  required
-                ></textarea>
-              </div>
-              <button
-                className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-3 px-6 rounded-full shadow-lg mr-4"
-                type="submit"
-                disabled={isSending}
-              >
-                {isSending ? 'Sending...' : 'Send Message'}
-              </button>
-              {isSent && <p className="text-green-600">Message sent successfully!</p>}
-            </form>
+            <Fade triggerOnce={true}>
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">Send us a message</h3>
+              <form onSubmit={handleFormSubmit}>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                    <AiOutlineMail className="inline-block mr-2" />
+                    Email
+                  </label>
+                  <input
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+                    <AiOutlineSend className="inline-block mr-2" />
+                    Message
+                  </label>
+                  <textarea
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    id="message"
+                    name="message"
+                    rows="5"
+                    placeholder="Your message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                  ></textarea>
+                </div>
+                <button
+                  className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-3 px-6 rounded-full shadow-lg mr-4"
+                  type="submit"
+                  disabled={isSending}
+                >
+                  {isSending ? 'Sending...' : 'Send Message'}
+                </button>
+                {isSent && <p className="text-green-600">Message sent successfully!</p>}
+              </form>
+            </Fade>
           </div>
 
           <div className="bg-white rounded-lg shadow-md p-6">
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">Contact Information</h3>
-            <p className="text-gray-700 mb-2">
-              <AiOutlineHome className="inline-block mr-2" />
-              <span className="font-semibold">Address:</span> Habiganj
-            </p>
-            <p className="text-gray-700 mb-2">
-              <AiOutlinePhone className="inline-block mr-2" />
-              <span className="font-semibold">Phone:</span> +8801937985805
-            </p>
-            <p className="text-gray-700 mb-2">
-              <AiOutlineMail className="inline-block mr-2" />
-              <span className="font-semibold">Email:</span> mominulhauqe14@gmail.com
-            </p>
-            <p className="text-gray-700">
-              <span className="font-semibold">Working Hours:</span> 24/7 Days
-            </p>
+            <Fade triggerOnce={true} delay={200}>
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">Contact Information</h3>
+              <p className="text-gray-700 mb-2">
+                <AiOutlineHome className="inline-block mr-2" />
+                <span className="font-semibold">Address:</span> Habiganj
+              </p>
+              <p className="text-gray-700 mb-2">
+                <AiOutlinePhone className="inline-block mr-2" />
+                <span className="font-semibold">Phone:</span> +8801937985805
+              </p>
+              <p className="text-gray-700 mb-2">
+                <AiOutlineMail className="inline-block mr-2" />
+                <span className="font-semibold">Email:</span> mominulhauqe14@gmail.com
+              </p>
+              <p className="text-gray-700">
+                <span className="font-semibold">Working Hours:</span> 24/7 Days
+              </p>
+            </Fade>
           </div>
         </div>
       </div>
